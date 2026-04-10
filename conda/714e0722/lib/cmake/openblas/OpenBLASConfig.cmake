5b7aee978cafccc7988acbabb60e6c45d099a5f0
Jing Yu <jyu@epib-genstat5.erasmusmc.nl>
2026-04-10 06:04:11 +0200

    Add: conda folder

---
+++ conda/714e0722/lib/cmake/openblas/OpenBLASConfig.cmake
@@ -0,0 +1,3 @@
+SET(OpenBLAS_VERSION "0.2.19")
+SET(OpenBLAS_INCLUDE_DIRS /STEAP/.snakemake/conda/714e0722/include)
+SET(OpenBLAS_LIBRARIES /STEAP/.snakemake/conda/714e0722/lib/libopenblas.so)
---
+++ conda/d78357d7/lib/python3.6/site-packages/pybedtools/version.py
@@ -0,0 +1,4 @@
+
+# THIS FILE IS GENERATED FROM SETUP.PY
+version = '0.8.2'
+__version__ = version
